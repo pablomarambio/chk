10edf231df3c225aa5cfd700496b54d63981d9ab
Pablo Marambio <yo@pablomarambio.cl>
2013-10-24 12:51:20 -0300

    Sin squlite

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131018171351) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: true do |t|
     t.datetime "when"
     t.integer  "duration"
