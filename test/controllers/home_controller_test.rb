8bcd7f3904ddd01532fa536887d8ebfde4c3f659
Pablo Marambio <yo@pablomarambio.cl>
2013-11-01 00:04:03 -0300

    Added the second event to the home test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,7 +4,8 @@ describe HomeController do
   describe "index" do
     it "should display the upcoming events" do
       get :index
-      response.body.must_match /Über/
+      response.body.must_match /Über Event 1/
+      response.body.must_match /Super Event 2/
     end
   end
 end
