5ec41160716ec055f8fd57bc72197d9f9e6c7e51
Pablo Marambio <yo@pablomarambio.cl>
2013-11-12 17:39:07 -0300

    LI API keys for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,10 @@
-LINKEDIN_API_KEY = "qm9p1viz2gq7"
-LINKEDIN_API_SEC = "1gQqsRmpOF66ZPDl"
+if Rails.env.development? || Rails.env.test?
+	LINKEDIN_API_KEY = "qm9p1viz2gq7"
+	LINKEDIN_API_SEC = "1gQqsRmpOF66ZPDl"
+elsif Rails.env.production?
+	LINKEDIN_API_KEY = "39g8fpt8sm78"
+	LINKEDIN_API_SEC = "JNInCtYk4SpJs42u"
+end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :linkedin, LINKEDIN_API_KEY, LINKEDIN_API_SEC
